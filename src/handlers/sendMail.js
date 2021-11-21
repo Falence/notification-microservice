358cfe90d834fa0142def966bb40021578093714
Falence Lemungoh <falencelemungoh@gmail.com>
2021-11-21 11:48:47 +0100

    sending our first email

--- src/handlers/sendMail.js
+++ src/handlers/sendMail.js
@@ -1,6 +1,29 @@
+import AWS from 'aws-sdk';
+
+const ses = new AWS.SES({ region: 'eu-west-1' });
+
 async function sendMail(event, context) {
-  console.log(event);
-  return event;
+  try {
+    const result = await ses.sendEmail({
+      Source: 'falencelemungoh@gmail.com',
+      Destination: {
+        ToAddresses: ['falencepixelate@gmail.com', 'falence.lemungoh@ubuea.cm'],
+      },
+      Message: {
+        Body: {
+          Text: {
+            Data: 'Hello from Falence Pixelate!',
+          },
+        },
+        Subject: {
+          Data: 'Test Mail',
+        },
+      },
+    }).promise();
+    return result;
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 export const handler = sendMail;
